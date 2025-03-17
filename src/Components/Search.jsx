02c24ca8a42fba3aee2d9aeb29eb81966dc21438
Aaron <thatshakespeare2@gmail.com>
2025-03-17 18:19:01 +0000

    move fetch. add modal details.

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -62,22 +62,19 @@ const Search = () => {
           className='dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm'
         >
           <li>
-            <a onClick={() => setSelectedRegion('Africa')}>Africa</a>
+            <a >Africa</a>
           </li>
           <li>
-            <a onClick={() => setSelectedRegion('Americas')}>America</a>
+            <a >America</a>
           </li>
           <li>
-            <a onClick={() => setSelectedRegion('Asia')}>Asia</a>
+            <a >Asia</a>
           </li>
           <li>
-            <a onClick={() => setSelectedRegion('Europe')}>Europe</a>
+            <a >Europe</a>
           </li>
           <li>
-            <a onClick={() => setSelectedRegion('Oceania')}>Oceania</a>
-          </li>
-          <li>
-            <a onClick={() => setSelectedRegion('')}>All</a>
+            <a >Oceania</a>
           </li>
         </ul>
       </div>
@@ -85,97 +82,98 @@ const Search = () => {
       {/* filtered cards */}
 
       {filteredCards ? (
-    <div className='grid grid-cols-1 mx-6 md:mx-auto md:grid-cols-2 lg:grid-cols-4 gap-14'>
-    {data.map((item) => (
-      <div key={item.cca3}>
-        {/* main cards */}
-        <CardTemplate
-          item={item}
-          onClick={() =>
-            document
-              .getElementById(`my_modal_2-${item.name.common}`)
-              .showModal()
-          }
-        >
-          <figure>
-            <img
-              src={item.flags?.svg}
-              alt={item.name.common}
-              className='object-cover w-full h-40'
-            />
-          </figure>
-          <div className='card-body'>
-            <h2 className='card-title font-bold'>{item.name.common}</h2>
-            <p>Population: {item.population}</p>
-            <p>Region: {item.region}</p>
-            <p>Capital: {item.capital}</p>
-          </div>
-        </CardTemplate>
 
-        {/* modal intead of page */}
-        <dialog
-          id={`my_modal_2-${item.name.common}`}
-          className='modal'
-          onClick={(e) => {
-            if (e.target === e.currentTarget) {
-              document
-                .getElementById(`my_modal_2-${item.name.common}`)
-                .close();
-            }
-          }}
-        >
-          <div className='modal-box w-11/12 h-11/12 md:h-6/12 md:max-w-5xl md:max-h-2xl lg:max-w-7xl lg:max-h-3xl mx-auto'>
-          <div className='modal-action'>
-              <form method='dialog'>
-                <button
-                  className='btn btn-sm btn-soft rounded-lg drop-shadow-xl absolute left-6 top-10'
-                  onClick={() =>
-                    document
-                      .getElementById(`my_modal_2-${item.name.common}`)
-                      .close()
-                  }
-                >
-                  <X />
-                </button>
-              </form>
-            </div>
-
-            <DetailCardTemplate item={item}>
-              
-              <figure className='flex items-center w-2/3 rounded-lg drop-shadow-xl'>
+      <div className="flex flex-col">
+        <div className='grid grid-cols-1 mx-6 md:mx-auto md:grid-cols-2 lg:grid-cols-4 gap-14'>
+        {filteredCountries.map((item) => (
+          <div key={item.cca3}>
+            {/* main cards */}
+            <CardTemplate
+              item={item}
+              onClick={() =>
+                document
+                  .getElementById(`my_modal_2-${item.name.common}`)
+                  .showModal()
+              }
+            >
+              <figure>
                 <img
                   src={item.flags?.svg}
                   alt={item.name.common}
-                  className='object-cover w-full'
+                  className='object-cover w-full h-40'
                 />
               </figure>
-              <div className='flex flex-col w-full mx-auto space-y-8 py-8 pl-12'>
-                <h2 className='card-title font-bold text-3xl'>
-                  {item.name.common}
-                </h2>
-                <div className='flex w-full'>
-                  <div className='flex flex-col w-1/2'>
-                    <p>Native Name: {item.nativename}</p>
-                    <p>Population: {item.population}</p>
-                    <p>Region: {item.region}</p>
-                    <p>Sub Region: {item.subregion}</p>
-                    <p>Capital: {item.capital}</p>
-                  </div>
-                  <div className='flex flex-col w-1/2'>
-                    <p>Top Level Domain: {item.tld}</p>
-                    <p>Currencies:</p>
-                    <p>Languages:</p>
-                  </div>
-                </div>
-                <Borders borders={item.borders} />
-                
+              <div className='card-body'>
+                <h2 className='card-title font-bold'>{item.name.common}</h2>
+                <p>Population: {item.population}</p>
+                <p>Region: {item.region}</p>
+                <p>Capital: {item.capital}</p>
               </div>
-            </DetailCardTemplate>
+            </CardTemplate>
+            {/* modal intead of page */}
+            <dialog
+              id={`my_modal_2-${item.name.common}`}
+              className='modal'
+              onClick={(e) => {
+                if (e.target === e.currentTarget) {
+                  document
+                    .getElementById(`my_modal_2-${item.name.common}`)
+                    .close();
+                }
+              }}
+            >
+              <div className='modal-box w-11/12 h-11/12 md:h-6/12 md:max-w-5xl md:max-h-2xl lg:max-w-7xl lg:max-h-3xl mx-auto'>
+              <div className='modal-action'>
+                  <form method='dialog'>
+                    <button
+                      className='btn btn-sm btn-soft rounded-lg drop-shadow-xl absolute left-6 top-2 lg:top-10'
+                      onClick={() =>
+                        document
+                          .getElementById(`my_modal_2-${item.name.common}`)
+                          .close()
+                      }
+                    >
+                      <X />
+                    </button>
+                  </form>
+                </div>
+                <DetailCardTemplate item={item}>
+      
+                  <figure className='flex items-center justify-center w-full mx-auto lg:w-2/3 rounded-lg drop-shadow-xl'>
+                    <img
+                      src={item.flags?.svg}
+                      alt={item.name.common}
+                      className='object-cover w-full'
+                    />
+                  </figure>
+                  <div className='flex flex-col w-full mx-auto lg:space-y-8 lg:py-8 lg:pl-12'>
+                    <h2 className='card-title font-bold text-3xl text-center'>
+                      {item.name.common}
+                    </h2>
+                    <div className='flex w-full'>
+                      <div className='flex flex-col w-1/2'>
+                        <p>Native Name: {item.nativename}</p>
+                        <p>Population: {item.population}</p>
+                        <p>Region: {item.region}</p>
+                        <p>Sub Region: {item.subregion}</p>
+                        <p>Capital: {item.capital}</p>
+                      </div>
+                      <div className='flex flex-col w-1/2'>
+                        <p>Top Level Domain: {item.tld}</p>
+                        <p>Currencies:</p>
+                        <p>Languages:</p>
+                      </div>
+                    </div>
+                    <Borders data={data} borders={item?.borders} />
+                  </div>
+                </DetailCardTemplate>
+              </div>
+            </dialog>
+            
           </div>
-        </dialog>
+        ))}
+            </div>
       </div>
-    ))}
-  </div>
 ) : searchTerm && (
   <p className="text-xs">None found.</p>
 )}
